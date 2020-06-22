53c8bb38f284584cfa2dea4f57b9e2ebd55e7415
Joe Chen <joe@sourcegraph.com>
2020-06-22 19:01:24 +0800

    migrations: add comments

--- migrations/1528395685_alter_permissions_object_ids_to_intarray.down.sql
+++ migrations/1528395685_alter_permissions_object_ids_to_intarray.down.sql
@@ -10,6 +10,7 @@ ALTER TABLE user_permissions DROP COLUMN IF EXISTS object_ids;
 ALTER TABLE repo_pending_permissions DROP COLUMN IF EXISTS user_ids;
 ALTER TABLE user_pending_permissions DROP COLUMN IF EXISTS object_ids;
 
+-- Reset values to be binary format of empty roaring bitmaps
 ALTER TABLE user_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
 ALTER TABLE repo_permissions ADD COLUMN user_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
 ALTER TABLE repo_pending_permissions ADD COLUMN object_ids BYTEA NOT NULL DEFAULT '\x3a30000000000000';
