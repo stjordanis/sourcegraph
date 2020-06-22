ed07a6a90ee92f03c38742305b458d3c5cf72327
Joe Chen <joe@sourcegraph.com>
2020-06-22 19:09:56 +0800

    migrations: remove intarray extension because we are not using its index

--- migrations/1528395685_alter_permissions_object_ids_to_intarray.up.sql
+++ migrations/1528395685_alter_permissions_object_ids_to_intarray.up.sql
@@ -10,8 +10,6 @@ ALTER TABLE user_permissions DROP COLUMN IF EXISTS object_ids;
 ALTER TABLE repo_pending_permissions DROP COLUMN IF EXISTS user_ids;
 ALTER TABLE user_pending_permissions DROP COLUMN IF EXISTS object_ids;
 
-CREATE EXTENSION IF NOT EXISTS intarray;
-
 ALTER TABLE repo_permissions ADD COLUMN user_ids INT[] NOT NULL DEFAULT '{}';
 ALTER TABLE user_permissions ADD COLUMN object_ids INT[] NOT NULL DEFAULT '{}';
 ALTER TABLE repo_pending_permissions ADD COLUMN user_ids INT[] NOT NULL DEFAULT '{}';
